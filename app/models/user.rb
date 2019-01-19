b19e5272950aa8f691b69ef8e6ec5f6c5b9e4411
Master_Cloud <lsclike2016@hotmail.com>
2019-01-19 23:21:03 +1100

    adding front

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   include DeviseTokenAuth::Concerns::User
+
+  def self.create_user_for_google(data)
+    where(uid: data["email"]).first_or_initialize.tap do |user|
+      user.provider="google_oauth2"
+      user.uid=data["email"]
+      user.email=data["email"]
+      user.password=Devise.friendly_token[0,20]
+      user.password_confirmation=user.password
+      user.save!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/api/auth'
+  mount_devise_token_auth_for 'User', at: '/api/auth', skip: [:omniauth_callbacks]
+  post 'auth/request', to:'authorization#get_authorization'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
