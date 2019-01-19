984245b096b8295f4e19b10ddf435a7a24221150
Master_Cloud <lsclike2016@hotmail.com>
2019-01-19 18:30:54 +1100

    init

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  include DeviseTokenAuth::Concerns::User
+end
